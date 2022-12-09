c20943d78b935bd68f7f3b29b7f984827912097d
chuongduong <duongthuychuong@gmail.com>
2022-12-09 17:12:54 +0700

    fetch data

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,9 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SET_LOADING:
       return { ...state, isLoading: true };
+    case SET_STORIES:
+      const { hits, nbPages } = action.payload;
+      return { ...state, isLoading: false, hits, nbPages };
     default:
       throw new Error("Invalid action type: " + action.type);
   }
